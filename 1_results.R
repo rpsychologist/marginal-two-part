4cdc9449b5314a76c98e06442d95831cbcbcbe5e
rpsychologist <1629860+rpsychologist@users.noreply.github.com>
2019-09-09 07:53:04 +0000

    cleaning

--- 1_results.R
+++ 1_results.R
@@ -11,7 +11,6 @@ require(ggstance)
 require(cowplot)
 theme_set(theme_minimal())
 
-
 # Scenario 0) no effect & 1) control relapse
 res1 <- readRDS("data/sim1_sum.Rds")
 
@@ -44,9 +43,9 @@ theme_cust <- theme_minimal() +
 
 theme_set(theme_cust)
 
-
 res_sum <- res_sum %>% 
-    filter(!model %in% c("log+0.0001", "2P-logodds")) %>% 
+    filter(!model %in% c("log+0.0001", 
+                         "2P-logodds")) %>% 
     mutate(n1 = factor(n1, levels = c(11, 6, 3)),
            model = case_when(model == "2P-log" ~ "Two-part", 
                              model == "2P-diff" ~ "Two-part",
@@ -61,37 +60,46 @@ res_sum <- res_sum %>%
 
 # Cover
 p_cov_log <- res_sum %>% 
-    #filter(model != "log+1") %>% 
     filter(link == "log") %>% 
     arrange(desc(n1)) %>% 
-    
     ggplot(aes(CI_cover,model, color = n1)) + 
     geom_line(aes(group = rev(n1))) +
-    geom_point() + #position = position_dodgev(height = .3)
-    #geom_linerangeh(aes(xmin = CI_cover, xmax = 0.95), position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    annotate("rect", xmin = 0.925, xmax = 0.975, ymin = -Inf, ymax = Inf, alpha = 0.1) +
-    geom_vline(xintercept = 0.95, linetype = "dotted") +
-    facet_grid(n2 ~ lab, scales = "free_x") +
-    labs(x = "95% CI Coverage", y = NULL) +
+    geom_point() + 
+    annotate("rect", 
+             xmin = 0.925, 
+             xmax = 0.975, 
+             ymin = -Inf, 
+             ymax = Inf, 
+             alpha = 0.1) +
+    geom_vline(xintercept = 0.95, 
+               linetype = "dotted") +
+    facet_grid(n2 ~ lab, 
+               scales = "free_x") +
+    labs(x = "95% CI Coverage", 
+         y = NULL) +
     scale_color_viridis_d(direction = 1) 
 
 p_cov_response <- res_sum %>% 
-    #filter(model != "log+1") %>% 
     filter(link == "response") %>% 
     arrange(desc(n1)) %>% 
-    
     ggplot(aes(CI_cover,model, color = n1)) + 
     geom_line(aes(group = rev(n1))) +
-    geom_point() + #position = position_dodgev(height = .3)
-    #geom_linerangeh(aes(xmin = CI_cover, xmax = 0.95), position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    annotate("rect", xmin = 0.925, xmax = 0.975, ymin = -Inf, ymax = Inf, alpha = 0.1) +
-    geom_vline(xintercept = 0.95, linetype = "dotted") +
-    labs(x = "95% CI Coverage", y = NULL) +
-    facet_grid(n2 ~ lab, scales = "free_x") +
+    geom_point() + 
+    annotate("rect", 
+             xmin = 0.925, 
+             xmax = 0.975, 
+             ymin = -Inf, 
+             ymax = Inf, 
+             alpha = 0.1) +
+    geom_vline(xintercept = 0.95, 
+               linetype = "dotted") +
+    labs(x = "95% CI Coverage", 
+         y = NULL) +
+    facet_grid(n2 ~ lab, 
+               scales = "free_x") +
     scale_color_viridis_d(direction = 1) 
 
 legend <- get_legend(p_cov_log + theme(legend.position = "bottom"))
-#ggsave(filename = "figures/sim_legend.pdf", legend, useDingbats = FALSE)
 
 p_cov <- plot_grid(p_cov_log, 
                    p_cov_response, 
@@ -103,60 +111,66 @@ p_cov <- plot_grid(p_cov_log,
 p_power_log <- res_sum %>% 
     filter(link == "log", lab != 0) %>% 
     arrange(desc(n1)) %>% 
-    ggplot(aes(Power,model, color = n1)) + 
+    ggplot(aes(Power, 
+               model, 
+               color = n1)) + 
     geom_line(aes(group = rev(n1))) +
     geom_point() + 
-    #geom_linerangeh(aes(xmin = 0, xmax = Power), position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    #annotate("rect", xmin = 0.925, xmax = 0.975, ymin = -Inf, ymax = Inf, alpha = 0.1) +
-    geom_vline(xintercept = 0.8, linetype = "dotted") +
-    labs(x = "Power", y = NULL) +
-    facet_grid(n2~lab) +
+    geom_vline(xintercept = 0.8, 
+               linetype = "dotted") +
+    labs(x = "Power", 
+         y = NULL) +
+    facet_grid(n2 ~ lab) +
     scale_color_viridis_d(direction = 1) 
 
 p_power_response <- res_sum %>% 
-    filter(link == "response", lab != 0) %>% 
+    filter(link == "response", 
+           lab != 0) %>% 
     arrange(desc(n1)) %>% 
-    ggplot(aes(Power,model, color = n1)) + 
+    ggplot(aes(Power,
+               model, 
+               color = n1)) + 
     geom_line(aes(group = rev(n1))) +
     geom_point() + 
-    #geom_linerangeh(aes(xmin = 0, xmax = Power), position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    #annotate("rect", xmin = 0.925, xmax = 0.975, ymin = -Inf, ymax = Inf, alpha = 0.1) +
-    geom_vline(xintercept = 0.8, linetype = "dotted") +
+    geom_vline(xintercept = 0.8, 
+               linetype = "dotted") +
     labs(x = "Power", y = NULL) +
-    facet_grid(n2~lab) +
+    facet_grid(n2 ~ lab) +
     scale_color_viridis_d(direction = 1) 
 
-p_power <- plot_grid(p_power_log, p_power_response, ncol = 1, align = "v")
+p_power <- plot_grid(p_power_log,
+                     p_power_response, 
+                     ncol = 1, 
+                     align = "v")
 
 # RMSE
 p_RMSE_log <- res_sum %>%
     filter(link == "log") %>%
     arrange(desc(n1)) %>% 
-    
-    ggplot(aes(RMSE, model, color = factor(n1))) +
+    ggplot(aes(RMSE, 
+               model, 
+               color = factor(n1))) +
     geom_line(aes(group = rev(n1))) +
     geom_point() +
-    #geom_linerangeh(aes(xmin = 0, xmax = RMSE), position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    #annotate("rect", ymin = 0.925, ymax = 0.975, xmin = -Inf, xmax = Inf, alpha = 0.1) +
-    #geom_hline(yintercept = 0.95, linetype = "dotted") +
-    facet_grid(n2~lab, scales = "free") +
-    labs(x = "RMSE", y = NULL) +
+    facet_grid(n2 ~ lab, 
+               scales = "free") +
+    labs(x = "RMSE", 
+         y = NULL) +
     scale_color_viridis_d(direction = 1)
 
 
 p_RMSE_response <- res_sum %>% 
     filter(link == "response") %>% 
     arrange(desc(n1)) %>% 
-    
-    ggplot(aes(RMSE, model, color = factor(n1))) + 
+    ggplot(aes(RMSE,
+               model, 
+               color = factor(n1))) + 
     geom_line(aes(group = rev(n1))) +
     geom_point() + 
-    # geom_linerangeh(aes(xmin = 0, xmax = RMSE),
-    #                 position = position_dodgev(height = .3), alpha = alpha_linerange) +
-    #annotate("rect", ymin = 0.925, ymax = 0.975, xmin = -Inf, xmax = Inf, alpha = 0.1) +
-    #geom_hline(yintercept = 0.95, linetype = "dotted") +
-    facet_grid(n2~lab, scales = "free") +
-    labs(x = "RMSE", y = NULL) +
+    facet_grid(n2 ~ lab, 
+               scales = "free") +
+    labs(x = "RMSE", 
+         y = NULL) +
     scale_color_viridis_d(direction = 1) 
 
 p_RMSE <- plot_grid(p_RMSE_log, 
@@ -174,28 +188,6 @@ p_RB_log <- res_sum %>%
     ggplot(aes(est_mean_RB, model, color = n1)) + 
     geom_line(aes(group = rev(n1))) +
     geom_point() + 
-
-    # geom_linerangeh(aes(xmin = 0,
-    #                     xmax = est_mean_RB), 
-    #                 position = position_dodgev(height = .3), 
-    #                 alpha = alpha_linerange) +
-    annotate("rect", xmin = -0.05, xmax = 0.05, ymin = -Inf, ymax = Inf, alpha = 0.1) +
-    geom_vline(xintercept = 0, linetype = "dotted") +
-    labs(x = "Relative bias", y = NULL) +
-    facet_grid(n2~lab) +
-    scale_color_viridis_d(direction = 1) 
-
-p_RB_response <- res_sum %>% 
-    filter(link == "response", lab != 0) %>% 
-    mutate(est_mean_RB = case_when(theta == 0 ~ est_mean,
-                                   TRUE ~ est_mean_RB)) %>% 
-    arrange(desc(n1)) %>% 
-    ggplot(aes(est_mean_RB, model, color = n1)) + 
-
-    geom_line(aes(group = rev(n1))) +
-    geom_point() + 
-    # geom_linerangeh(aes(xmin = 0, xmax = est_mean_RB), position = position_dodgev(height = .3),
-    #                 alpha = alpha_linerange) +
     annotate("rect", 
              xmin = -0.05, 
              xmax = 0.05, 
@@ -204,8 +196,33 @@ p_RB_response <- res_sum %>%
              alpha = 0.1) +
     geom_vline(xintercept = 0, 
                linetype = "dotted") +
-    facet_grid(n2 ~ lab, scales = "free") +
-    labs(x = "Relative bias", y = NULL) +
+    labs(x = "Relative bias", 
+         y = NULL) +
+    facet_grid(n2 ~ lab) +
+    scale_color_viridis_d(direction = 1) 
+
+p_RB_response <- res_sum %>% 
+    filter(link == "response", lab != 0) %>% 
+    mutate(est_mean_RB = case_when(theta == 0 ~ est_mean,
+                                   TRUE ~ est_mean_RB)) %>% 
+    arrange(desc(n1)) %>% 
+    ggplot(aes(est_mean_RB, 
+               model, 
+               color = n1)) + 
+    geom_line(aes(group = rev(n1))) +
+    geom_point() + 
+    annotate("rect", 
+             xmin = -0.05, 
+             xmax = 0.05, 
+             ymin = -Inf, 
+             ymax = Inf, 
+             alpha = 0.1) +
+    geom_vline(xintercept = 0, 
+               linetype = "dotted") +
+    facet_grid(n2 ~ lab, 
+               scales = "free") +
+    labs(x = "Relative bias", 
+         y = NULL) +
     scale_color_viridis_d(direction = 1) 
 
 p_RB <- plot_grid(p_RB_log, 
